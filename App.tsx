8ae68bf2eac49efdff348716d77b38c34fd229cb
lolicon <leminhtien2892004@gmail.com>
2025-09-30 15:36:20 +0700

    View, Text, Styles

--- App.tsx
+++ App.tsx
@@ -2,14 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  //jsx
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text style={styles.header}>hoidanit2</Text>
+        <Text style={styles.parent}>
+          hoidanit1
+          <Text style={styles.child}>bla bla</Text>
+        </Text>
+      </View>
+      <Text style={styles.hello1}>Hello World 1</Text>
+      <Text>Hello World 2</Text>
     </View>
   );
 }
 
+
+// ko có khái niệm css
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +27,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  hello1: {
+    color: 'red',
+    fontSize: 60,
+    borderColor: 'green',
+    borderWidth: 1,
+    padding: 10
+  },
+  header: {
+    fontSize: 30,
+    fontWeight: "600"
+  },
+  parent: {
+    fontSize: 60,
+    color: 'green'
+  },
+  child: {
+    fontSize: 30,
+    color: 'pink'
+  }
 });
