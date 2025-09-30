1a05602e80e2c1e24e5637babc8864946e502a4d
lolicon <leminhtien2892004@gmail.com>
2025-09-30 17:02:07 +0700

    Text inputs

--- App.tsx
+++ App.tsx
@@ -1,13 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
 
   const [count, setCount] = useState<number>(0);
+  const [name, setName] = useState<string>("");
+  const [age, setAge] = useState<number>(0);
+
+
   //jsx
   return (
     <View style={styles.container}>
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Name:{name}</Text>
+        <TextInput
+          multiline
+          onChangeText={(value) => setName(value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+        />
+      </View>
+      <View>
+        <Text style={{ fontSize: 40, fontWeight: "600" }}>Age:{age}</Text>
+        <TextInput
+          onChangeText={(value) => setAge(+value)}
+          style={{
+            borderColor: "green",
+            borderWidth: 1,
+            width: 200,
+            padding: 15
+          }}
+          keyboardType='numeric'
+          maxLength={2}
+        />
+      </View>
       <Text style={{ fontSize: 40, fontWeight: "600" }}>
         count={count}
       </Text>
@@ -17,7 +48,7 @@ export default function App() {
           title='Increase' onPress={() => setCount(count + 1)}
         />
       </View>
-    </View>
+    </View >
   );
 }
 
